a94eee050b91a49083f6a95eb8410543faa80fc9
mtaketani113 <take1131012@gmail.com>
2021-04-24 01:10:25 +0900

    implement component with connect function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 
-export cost increment = () => ({
+export const increment = () => ({
     type: 'INCREMENT'
 })
 
-export cost decrement = () => ({
+export const decrement = () => ({
     type: 'DECREMENT'
 })
